8d634cf38040177f0b73fd2065a958a1875ae887
snape1112 <107560795+snape1112@users.noreply.github.com>
2022-11-08 21:43:50 +0200

    complete

--- test_plans/test_checkout.py
+++ test_plans/test_checkout.py
@@ -1,16 +1,19 @@
 import pytest
 import uuid
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 import re
-import variables
 from time import sleep
 
 @pytest.mark.regression
 def test_checkout(page):
-    page.goto('https://utility-marketplace-store.myshopify.com')
+    # site_url = 'https://utility-marketplace-store.myshopify.com'
+    site_url = 'https://dte-business-marketplace.myshopify.com/'
+
+    page.goto(site_url)
 
     # input password
-    page.fill('#password', 'mifrap')
+    # page.fill('#password', 'mifrap')
+    page.fill('#password', 'DTE2022')
     page.click('text="Enter"')
 
     # go to list page
@@ -33,7 +36,7 @@ def test_checkout(page):
     page.click('#verifyAddressButton')
 
     # input contact info and shipping address
-    page.fill('#checkout_email_or_phone', 'hello@email.com')
+    page.fill('#checkout_email', 'hello@email.com')
     page.fill('#checkout_shipping_address_first_name', 'A')
     page.fill('#checkout_shipping_address_last_name', 'B')
     page.fill('#checkout_shipping_address_address1', '1250 Ocean pkwy')
@@ -44,11 +47,29 @@ def test_checkout(page):
     page.click('text="Continue to shipping"')
 
     # select shipping method
-    page.click('#checkout_shipping_rate_id_shopify-economy-4_90')
+    page.click('#checkout_shipping_rate_id_shopify-economy-0_00')
     page.click('text="Continue to payment"')
 
     # input credit card
-    iframe_element = page.wait_for_selector("#card-fields-number-p31q2xvxoo000000-scope-utility-marketplace-store.myshopify.com")
+    iframe_element = page.wait_for_selector(".card-fields-iframe")
     iframe = iframe_element.content_frame()
+    sleep(3)
+    iframe.fill('#number', '4242424242424242')
+    sleep(3)    
+    iframe.fill('#name', 'Bogus Gateway')
+    sleep(3)
+    iframe.fill('#expiry', '03/25')
+    sleep(3)    
+    iframe.fill('#verification_value', '333')
+    page.click('#checkout_different_billing_address_false')
+    page.click('text="Pay now"')
 
-    sleep(100000)
+    # verify order number and thank you, address
+    page.wait_for_selector(".os-order-number")
+    expect(page.locator('.os-order-number')).to_contain_text('Order #')
+    expect(page.locator('#main-header')).to_have_text('Thank you A!')
+    expect(page.locator('.section__content__column--half:first-child .address')).to_contain_text('Brooklyn')
+    expect(page.locator('.section__content__column--half:nth-child(2) .address')).to_contain_text('Brooklyn')
+    page.click('text="Continue shopping"')
+
+    expect(page).to_have_url(site_url)
--- variables.py
+++
@@ -1,9 +0,0 @@
-subdomain = None 
-base_url = None 
-account_email = None 
-admin_name = None 
-token = None
-business_name = None
-
-
- 
